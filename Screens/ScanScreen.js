15ac20cda5b3df96b6fa4402c31f0dd210fe1db9
SHANKHA-SUBHA-PAL <subhankarpal76@gmail.com>
2021-04-24 10:44:46 +0530

    FIXED I THINK

--- Screens/ScanScreen.js
+++ Screens/ScanScreen.js
@@ -29,6 +29,8 @@ getCameraPermissions = async (id) =>{
   }
 
   handleBarCodeScanned = async({type, data})=>{
+   const{buttonState} = this.state
+   
     this.setState({
       scanned:true,
       scannedData: data,
@@ -40,10 +42,11 @@ render(){
     const hasCameraPermissions = this.state.hasCameraPermissions;
       const scanned = this.state.scanned;
       const buttonState = this.state.buttonState;
-if(buttonState === "clicked" && hasCameraPermissions){
+if(buttonState !== "normal" && hasCameraPermissions){
   return(
     <BarCodeScanner
     onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+    style={StyleSheet.absoluteFillObject}
     />
   )
 }
@@ -84,3 +87,42 @@ const styles = StyleSheet.create({
 
 
 })
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    displayText:{
+      fontSize: 15,
+      textDecorationLine: 'underline'
+    },
+    scanButton:{
+      backgroundColor: '#2196F3',
+      padding: 10,
+      margin: 10
+    },
+    buttonText:{
+      fontSize: 15,
+      textAlign: 'center',
+      marginTop: 10
+    },
+    inputView:{
+      flexDirection: 'row',
+      margin: 20
+    },
+    inputBox:{
+      width: 200,
+      height: 40,
+      borderWidth: 1.5,
+      borderRightWidth: 0,
+      fontSize: 20
+    },
+    scanButton:{
+      backgroundColor: '#66BB6A',
+      width: 50,
+      borderWidth: 1.5,
+      borderLeftWidth: 0
+    }
+  });
